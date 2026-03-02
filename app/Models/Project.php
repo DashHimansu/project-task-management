45f0678f694f3f7468a57096fa6df0474b3b5526
Himansu Dash <87652836+DashHimansu@users.noreply.github.com>
2026-03-02 08:53:17 +0530

    uncomment the hasfactory

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Project extends Model
 {
+     use HasFactory;
      protected $fillable = [
         'name','description','start_date','end_date'
     ];
